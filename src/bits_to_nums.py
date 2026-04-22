f9d4f0b3051eb83b4618e155a78a091ea3cf5303
Kirill Shmakov <shmakir@yandex.ru>
2026-04-22 23:33:10 +0200

    Add initialization and logging.

--- src/bits_to_nums.py
+++ src/bits_to_nums.py
@@ -3,11 +3,17 @@ import torch.nn as nn
 
 
 class BitsToNumsNet(nn.Module):
-    def __init__(self, bits: int, nums: int, hidden: int = 10):
+    def __init__(self, bits: int, nums: int, seed: int = 239,  hidden: int = 10):
         super().__init__()
         self.fc1 = nn.Linear(bits, hidden)
         self.act = nn.ReLU()
         self.fc2 = nn.Linear(hidden, nums)
+
+        torch.manual_seed(seed)
+
+        for layer in [self.fc1, self.fc2]:
+            nn.init.normal_(layer.weight, mean=0.0, std=0.02)
+            nn.init.normal_(layer.bias, mean=0.0, std=0.02)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.fc1(x)
